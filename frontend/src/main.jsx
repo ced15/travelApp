98a8e1d6ff3939887c858acd250ea52446684fc2
Denisa Cuta <denisa1506@yahoo.com>
2023-10-10 12:09:58 +0300

    !

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import LogIn from "./Components/LogIn/LogIn";
 import SignUp from './Components/SignUp/SignUp';
-import "./index.css"
-import SignUp from "./Components/SignUp/SignUp";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
