8683adb906717399cba30c7d2624a239bed89742
Denisa Cuta <denisa1506@yahoo.com>
2023-11-22 12:37:16 +0200

    Changes by Cristi

--- backend/src/main/java/com/example/demo/components/User.java
+++ backend/src/main/java/com/example/demo/components/User.java
@@ -3,16 +3,19 @@ package com.example.demo.components;
 import com.example.demo.components.Trip;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
 
-@Getter
-@Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 @Entity
 @Table(name = "_user")
 public class User implements UserDetails {
