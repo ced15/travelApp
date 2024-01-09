ab9df1cd1b1810519d3f6eb79c369ed6c2849d9a
Paaul2121 <paulcosteacristian@yahoo.com>
2024-01-09 11:26:57 +0200

    env

--- frontend/src/Components/Homepage/Homepage.jsx
+++ frontend/src/Components/Homepage/Homepage.jsx
@@ -1,33 +1,54 @@
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, useLoadScript } from '@react-google-maps/api';
 import "./Homepage.css";
 import Header from "./Header/Header";
+import React, { useRef, useEffect } from 'react';
+import Loading from '../Loading/Loading';
 
 const Homepage = () => {
-   const mapStyles = {
-    height: '100vh',
-    width: '100%',
-  };
 
-  const defaultCenter = {
-    lat: 40.748817,
-    lng: -73.985428,
-  };
+  const env = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+  const  { isLoaded } = useLoadScript({
+    googleMapsApiKey: env,
+    libraries: ["places"]
+  });
+
+  if(!isLoaded) {
+    return (
+      <Loading />
+    )
+  }
+
+  // const mapStyles = {
+  //   height: '100vh',
+  //   width: '100%',
+  // };
+
+  // const defaultCenter = {
+  //   lat: 40.748817,
+  //   lng: -73.985428,
+  // };
+
+  // return (
+    
+  //   <div>
+  //     <Header/>
+  //     <LoadScript googleMapsApiKey="AIzaSyD_5tG3mD_cqZUW2uWPFoC1r8XZT2d8WxE">
+  //     <div>
+  //         <GoogleMap mapContainerStyle={mapStyles} zoom={13} center={defaultCenter} />
+  //       </div>
+  //     </LoadScript>
+      
+  //   </div>
+  // );
 
   return (
     <div>
       <Header/>
-    <LoadScript
-      googleMapsApiKey="AIzaSyD_5tG3mD_cqZUW2uWPFoC1r8XZT2d8WxE"
-    >
-      <GoogleMap
-        mapContainerStyle={mapStyles}
-        zoom={13}
-        center={defaultCenter}
-      />
-    </LoadScript>
+        <div> Map 
+
+        </div>
     </div>
   )
-}
-
+};
 
 export default Homepage;
