39006173dfd0055ba97c2879e5e1a5ee9eeb10b7
Paaul2121 <paulcosteacristian@yahoo.com>
2024-01-30 13:34:20 +0200

    styling

--- frontend/src/Components/Map/Map.jsx
+++ frontend/src/Components/Map/Map.jsx
@@ -1,11 +1,11 @@
 import { useState, useMemo, useCallback, useRef } from "react";
 import Loading from "../Loading/Loading";
 import {
-    GoogleMap,
-    Marker,
-    DirectionsRenderer,
-    Circle,
-    MarkerClusterer,
+  GoogleMap,
+  Marker,
+  DirectionsRenderer,
+  Circle,
+  MarkerClusterer,
 } from "@react-google-maps/api";
 import "./Map.css";
 import Places from "./Places";
@@ -44,95 +44,130 @@ const Map = () => {
     setIsFormVisible(true);
   };
 
-    const handleAddMemento = () => {
-        setMemento("");
-    };
+  const handleAddLocation = () => {
+    location != null &&
+      setLocations((prevLocations) => [...prevLocations, location]);
+  };
 
-    const [date, setDate] = useState({
-        startDate: new Date(),
-        endDate: new Date().setMonth(11),
-    });
+  const handleAddMemento = () => {
+    setMemento("");
+  };
 
-    const handleValueChange = (newValue) => {
-        console.log("newValue:", newValue);
-        setDate(newValue);
-    };
+  const [departureDate, setDepartureDate] = useState({
+    Date: new Date(),
+  });
 
-    return (
-        <div className="flex h-screen">
-            <div className="w-1/6 p-4 bg-black text-white">
-                <h1 className="font-semibold italic pb-2"> Search for a location </h1>
-                <Places
-                    setLocation={(position) => {
-                        setLocation(position);
-                        mapRef.current?.panTo(position);
-                    }}
+  const [arrivalDate, setArrivalDate] = useState({
+    Date: new Date(),
+  });
+
+  const handleDepartureDateChange = (newValue) => {
+    setDepartureDate((prevDate) => ({
+      ...prevDate,
+      startDate: newValue.startDate,
+    }));
+    handleInputChange("departureDate", newValue.startDate);
+  };
+
+  const handleArrivalDateChange = (newValue) => {
+    setArrivalDate((prevDate) => ({
+      ...prevDate,
+      endDate: newValue.startDate,
+    }));
+    handleInputChange("arrivalHomeDate", newValue.startDate);
+  };
+
+  return (
+    <div className="flex h-screen">
+      <div className="w-1/6 p-4 bg-black text-white">
+        <h1 className="font-semibold italic pb-2"> Search for a location </h1>
+        <Places
+          setLocation={(position) => {
+            setLocation(position);
+            mapRef.current?.panTo(position);
+          }}
+        />
+        <button
+          className="flex flex-row items-center rounded-xl bg-black-100 py-3 text-base font-medium text-navy-700"
+          onClick={isFormVisible ? handleAddLocation : handleCreateTrip}
+        >
+          {isFormVisible ? "Add Location" : "Create Trip"}
+
+          {/* <MdChevronRight className="text-lg" /> */}
+        </button>
+
+        {isFormVisible && (
+          <div>
+            <form>
+              <label className="font-semibold italic">
+                Trip Name:
+                <br></br>
+                <div className="pb-1"></div>
+                <input className="w-full text-black"
+                  type="text"
+                  value={tripName}
+                  onChange={(e) => setTripName(e.target.value)}
                 />
-                <button
-                    className="flex flex-row items-center rounded-xl bg-black-100 px-4 py-3 text-base font-medium text-navy-700 transition duration-200 hover:bg-gray-200 active:bg-gray-300 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 dark:active:bg-white/30"
-                    onClick={isFormVisible ? handleAddLocation : handleCreateTrip}
-                >
-                    {isFormVisible ? "Add Location" : "Create Trip"}
-
-                    {/* <MdChevronRight className="text-lg" /> */}
-                </button>
-
-                {isFormVisible && (
-                    <div>
-                        <form>
-                            <label>
-                                Trip Name:
-                                <input
-                                    type="text"
-                                    value={tripName}
-                                    onChange={(e) => setTripName(e.target.value)}
-                                />
-                            </label>
-                            <br></br>
-                            <br></br>
-                            <label>
-                                Locations:
-                                <ul>
-                                    {locations.map((location, index) => (
-                                        <li key={index}>{location.name}</li>
-                                    ))}
-                                </ul>
-                            </label>
-                            <br></br>
-
-                            <label>
-                                Add Memento:
-                                <input
-                                    type="text"
-                                    value={memento}
-                                    onChange={(e) => setMemento(e.target.value)}
-                                />
-                            </label>
-                            <br></br>
-                            <br></br>
-                            <label>
-                                Select Departure and Arrival date
-                                <Datepicker value={date} onChange={handleValueChange} />
-                            </label>
-                        </form>
-                        <button>Save trip</button>
-                    </div>
-                )}
-            </div>
+              </label>
+              <br></br>
+              <br></br>
+              <label className="font-semibold italic ">
+                Locations:
+                <ul>
+                  {locations.map((location, index) => (
+                    <li key={index}>{location.name}</li>
+                  ))}
+                </ul>
+              </label>
+              <br></br>
+              <label className="font-semibold italic">
+                Add Memento:
+                <br></br>
+                <div className="pb-1"></div>
+                <textarea className="w-full text-black h-24 pl-0.5"
+                  type="text"
+                  value={memento}
+                  onChange={(e) => setMemento(e.target.value)}
+                />
+              </label>
+              <br></br>
+              <br></br>
+              <label className="font-semibold italic">               
+                Select Departure Date:
+                <div className="pb-1"></div>
+                <Datepicker className="text-black"
+                  value={departureDate}
+                  onChange={handleDepartureDateChange}
+                />
+              </label>
+              <br />
+              <label className="font-semibold italic">
+                Select Arrival Date:
+                <div className="pb-1"></div>
+                <Datepicker className="text-black"
+                  value={arrivalDate}
+                  onChange={handleArrivalDateChange}
+                />
+              </label>
+            </form>
+            <button className="pt-2 font-semibold italic text-lg place-content-center">Save trip</button>
+          </div>
+        )}
+      </div>
 
 
 
-            <div className="h-screen w-full">
-                <GoogleMap
-                    zoom={15}
-                    center={center}
-                    mapContainerClassName="h-screen w-full"
-                    options={options}
-                    onLoad={onLoad}>
-                    {location && <Marker key='0' icon={image.url} position={location} />}
-                </GoogleMap>
-            </div>
-        </div>
-    )
+      <div className="h-screen w-full">
+        <GoogleMap
+          zoom={15}
+          center={center}
+          mapContainerClassName="h-screen w-full"
+          options={options}
+          onLoad={onLoad}>
+          {location && <Marker key='0' icon={image.url} position={location} />}
+        </GoogleMap>
+      </div>
+    </div>
+  )
 }
 export default Map;
